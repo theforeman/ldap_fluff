4c08a1c4b8772fcfc4cc3357e260f6b4b875b9a0
c-silva <silvadisto@gmail.com>
2016-06-06 18:11:35 +0100

    Prevent infinite recurse when 2 groups are members of each other. Tries to solve issue already reported: https://github.com/theforeman/ldap_fluff/issues/51
    Fix _walk_group_ancestry method declaration with extra variable
    Refer unused variable with _
    Lineup equals signs

--- lib/ldap_fluff/ad_member_service.rb
+++ lib/ldap_fluff/ad_member_service.rb
@@ -19,25 +19,28 @@ class LdapFluff::ActiveDirectory::MemberService < LdapFluff::GenericMemberServic
   def _groups_from_ldap_data(payload)
     data = []
     if !payload.nil?
-      first_level  = payload[:memberof]
-      total_groups = _walk_group_ancestry(first_level)
-      data         = (get_groups(first_level + total_groups)).uniq
+      first_level     = payload[:memberof]
+      total_groups, _ = _walk_group_ancestry(first_level, first_level)
+      data            = (get_groups(first_level + total_groups)).uniq
     end
     data
   end
 
   # recursively loop over the parent list
-  def _walk_group_ancestry(group_dns = [])
+  def _walk_group_ancestry(group_dns = [], known_groups = [])
     set = []
     group_dns.each do |group_dn|
       search = @ldap.search(:base => group_dn, :scope => Net::LDAP::SearchScope_BaseObject, :attributes => ['memberof'])
       if !search.nil? && !search.first.nil?
-        group = search.first
-        set  += _walk_group_ancestry(group[:memberof])
-        set  += group[:memberof]
+        groups                       = search.first[:memberof] - known_groups
+        known_groups                += groups
+        next_level, new_known_groups = _walk_group_ancestry(groups, known_groups)
+        set                         += next_level
+        set                         += groups
+        known_groups                += next_level
       end
     end
-    set
+    return [set, known_groups]
   end
 
   def class_filter
